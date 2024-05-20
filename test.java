dea021f42105cc3d0074aa8811a8baca5c8547aa
EswarVallabha <eswar.r@zohocorp.com>
2024-05-20 15:39:16 +0530

    Prac

--- test.java
+++ test.java
@@ -1,6 +1,21 @@
 This is a test and practice for pull req to check how it works.
+<<<<<<< HEAD
 dnc
   dcjb
   dxbjk
   dcuj
 hdc
+=======
+
+
+edub
+saujd
+daha
+kucd
+bhjfs
+dskbd
+sjbdss
+sdkbds
+sbsf
+
+>>>>>>> aeab223 (Prac)
